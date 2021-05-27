9c92a36f896b16820a16d9b3427b32952450aa75
Ahmet Yesertener <info@ayesoft.net>
2021-05-27 22:02:29 +0300

    Initial Commit

---
+++ GitTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitTestProject
+//
+//  Created by Ahmet Yeşertener on 27.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
